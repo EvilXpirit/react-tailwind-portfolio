bec79aebd79bd0f57994267ec20f33c18cd6f0d8
ADitya SHarma <adsharma122@gmail.com>
2024-08-15 02:40:09 +0530

    experience update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,29 +5,53 @@ import colorSharp from "../assets/img/color-sharp2.png";
 
 export const Experience = () => {
   const experiences = [
-    "Front-end Web Development",
-    "Machine Learning",
-    "Python Programming",
-    "Java Programming",
-    "UI/UX",
-    "Graphic Designing",
-    "Illustration",
-    // more experiences
+    {
+      position: "Software Developer Intern",
+      company: "Numetry Technologies",
+      duration: "6 Months",
+      from: "Jan 2024 - July 2024",
+      description: [
+        " Gained hands-on experience with the MERN stack (MongoDB, ExpressJS, ReactJS, Node.js). ",
+        "Developed and maintained responsive web applications",
+        " Worked on multiple frontend-based mini projects as the part of training. Collaborated with the software development team on various projects, including live projects, enhancing both frontend and backend functionalities.",
+        " Handled different RESTful APIs. ",
+        "Demonstrated strong problem-solving skills and the ability to adapt to new technologies quickly.",
+        " Received positive feedback for a high level of comprehension, efficient task management, and professional demeanor."
+      ],
+    },
+    {
+      position: " Machine Learning Intern",
+      company: "Bharat Intern",
+      duration: "1 Month",
+      from: "Aug 2023 - Sep 2023",
+      description: [
+        " Built a machine learning model to predict house prices using linear regression.",
+        "Developed a machine learning model to predict the quality of wine using linear regression.",
+        "Created a model to predict different species of flowers based on the length of their petals and sepals using logistic regression."
+      ],
+    },
+    {
+      position: "Web Developer Intern",
+      company: "TechnoHacks EduTech",
+      duration: "1 Month",
+      from: "July 2023 - Aug 2023",
+      description: [
+        " Built a Responsive Portfolio website using React.js",
+        "Created a web application that allows users to convert currency amounts between different currencies using real-time exchange rates using HTML, CSS & JavaScript.",
+        "Built A Calculator web application using Html, CSS and JavaScript that performs basic arithmetic operations."
+      ],
+    },
   ];
 
   return (
-    <section className="experience" id="experience">
-      <div className="container">
-        <div className="row">
+    <section className="experience position-relative pb-5" id="experience">
+      <div className="container mx-auto">
           <div className="col-12">
-            <div className="experience-bx wow zoomIn">
-              <h2>Experience</h2>
-              {/* <p>While I have not had any direct experience.</p> */}
-        {/* Use the ExperienceList component here */}
-        <ExperienceList experiences={experiences} />
+            <div className="experience-bx wow zoomIn text-center -mt-14 px-12 py-14">
+              <h2 className="pb-10">Experience</h2>
+              <ExperienceList experiences={experiences} />
             </div>
           </div>
-        </div>
       </div>
       <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -3,11 +3,25 @@ import React from "react";
 const ExperienceList = ({ experiences }) => {
   return (
     <div className="experience-list">
-      {/* <h3>Work Experience</h3> */}
-      <ul>
+      <ul className="space-y-8">
         {experiences.map((experience, index) => (
-          <li key={index}>
-            <p>{experience}</p>
+          <li key={index} className="flex flex-col-reverse md:flex-row items-start md:items-center">
+            <div className="md:w-1/4 text-left lg:text-center flex md:flex-col flex-col-reverse">
+              <p className="text-lg font-medium text-gray-600">{experience.duration}</p>
+              <p className="text-sm text-gray-400 mt-3 md:mt-4">{experience.from}</p>
+            </div>
+            <div className="md:w-1/12 flex justify-center items-center">
+              <div className="h-20 md:h-[250px] w-px bg-gradient-to-b from-transparent via-gray-500 to-transparent opacity-50 hidden md:flex"></div>
+            </div>
+            <div className="md:w-2/3 text-start">
+              <h4 className="text-xl font-semibold">{experience.position}</h4>
+              <h5 className="text-lg text-gray-500">{experience.company}</h5>
+              <ul className="list-disc pl-5 text-gray-500">
+                {experience.description.map((desc, i) => (
+                  <li key={i}>{desc}</li>
+                ))}
+              </ul>
+            </div>
           </li>
         ))}
       </ul>
